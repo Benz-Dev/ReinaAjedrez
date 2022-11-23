db6d721061fbc852fa8099744bb6c9f3fec66af4
Jose Manuel <54948617+Benz-Dev@users.noreply.github.com>
2022-11-23 21:05:14 +0100

    Reina Constructor

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -2,5 +2,37 @@ package org.iesalandalus.programacion.reinaajedrez.modelo;
 
 public class Reina {
     private Color color;
-    
+    private Posicion posicion;
+
+    Reina(){
+        this.color = Color.BLANCO;
+        this.posicion= new Posicion(1, 'd');
+    }
+
+    // COLOR
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        if (Color.BLANCO == color || Color.NEGRO == color){
+            this.color = color;
+        }else {
+            throw new IllegalArgumentException("El color no es el esperado.");
+        }
+    }
+
+    // POSICION
+    public Posicion getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(Posicion posicion) {
+        try {
+            this.posicion = posicion;
+        } catch (IllegalArgumentException e){
+            throw e;
+        }
+
+    }
 }
