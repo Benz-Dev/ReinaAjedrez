dd4ed62d15e388343d4b2924874b728314c79b26
Jose Manuel <54948617+Benz-Dev@users.noreply.github.com>
2022-11-23 19:58:14 +0100

    Enum `Color` - toString

---
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Color.java
@@ -0,0 +1,17 @@
+package org.iesalandalus.programacion.reinaajedrez.modelo;
+
+public enum Color {
+    BLANCO("Blanco"),
+    NEGRO("Negro");
+
+    public final String cadenaAMostrar;
+
+    Color(String cadenaAMostrar) {
+        this.cadenaAMostrar = cadenaAMostrar;
+    }
+
+    @Override
+    public String toString() {
+        return this.cadenaAMostrar;
+    }
+}
