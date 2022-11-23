d48d43bffb5e1a7026440e56bac95d75934fff85
Jose Manuel <54948617+Benz-Dev@users.noreply.github.com>
2022-11-23 22:46:21 +0100

    .

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
@@ -1,7 +1,25 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
 import org.iesalandalus.programacion.reinaajedrez.modelo.*;
+import org.iesalandalus.programacion.utilidades.Entrada;
 
 public class Consola {
- 
+    public Consola(){}
+
+    public static void mostarMenu(){
+        System.out.println("1. Crear una reina por defecto.");
+        System.out.println("2. Crear una reina eligiendo el color.");
+        System.out.println("3. Mover una reina.");
+        System.out.println("4. Salir.");
+    }
+
+    public int elegirOpcionMenu(){
+        int opcion = 0;
+        do{
+            System.out.println("Elige una opción: ");
+            opcion = Entrada.entero();
+        }while(opcion < 1 || opcion > 4);
+        return opcion;
+    }
+    public
 }
