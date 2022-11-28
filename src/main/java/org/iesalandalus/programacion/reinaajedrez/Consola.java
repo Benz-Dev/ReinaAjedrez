2062b98de026da7158abeedd6fe7d09d5c0f02ed
Jose Manuel <54948617+Benz-Dev@users.noreply.github.com>
2022-11-28 17:52:33 +0100

    Despedirse

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
@@ -32,4 +32,38 @@ public class Consola {
 
         return opcion==1?Color.BLANCO:Color.NEGRO;
     }
+
+    public void mostrarMenuDirecciones(){
+        System.out.println("Elige una opción: ");
+        System.out.println("N. Norte.");
+        System.out.println("S. Sur.");
+        System.out.println("E. Este.");
+        System.out.println("O. Oeste.");
+        System.out.println("NE. Nordeste.");
+        System.out.println("NO. Noreste.");
+        System.out.println("SE. Sureste.");
+        System.out.println("SO. Suroeste.");
+    }
+
+    public Direccion elegirDireccion(){
+        String opcion = "";
+        do{
+           System.out.println("Elige una opción: ");
+            opcion = Entrada.cadena();
+        }while(opcion.equals("N") || opcion.equals("S") || opcion.equals("E") || opcion.equals("O") || opcion.equals("NE") || opcion.equals("NO") || opcion.equals("SE") || opcion.equals("SO"));
+        return opcion.equals("N")?Direccion.NORTE:opcion.equals("S")?Direccion.SUR:opcion.equals("E")?Direccion.ESTE:opcion.equals("O")?Direccion.OESTE:opcion.equals("NE")?Direccion.NORESTE:opcion.equals("NO")?Direccion.NOROESTE:opcion.equals("SE")?Direccion.SURESTE:opcion.equals("SO")?Direccion.SUROESTE:null;
+    }
+
+    public int elegirPasos(){
+        int pasos = 0;
+        do{
+            System.out.println("Elige una opción: ");
+            pasos = Entrada.entero();
+        }while(pasos < 1);
+        return pasos;
+    }
+
+    public void despedirse(){
+        System.out.println("Hasta pronto.\nAjedrez terminado");
+    }
 }
